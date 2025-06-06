b43f9e9b7066506bf03347eaa02d67ccc443956d
akash <devadigaakash777@gmail.com>
2025-06-06 10:13:01 -0400

    Implement fetchDealers, fetchDealers by state, and fetchDealer by ID endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,37 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.status(200).json(dealers);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const state = req.params.state;
+        const dealers = await Dealerships.find({ state: state });
+        res.status(200).json(dealers);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const id = req.params.id;
+        const dealer = await Dealerships.find({ id: id });
+        if (!dealer) {
+          return res.status(404).json({ error: 'Dealer not found' });
+    }
+        res.status(200).json(dealer);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer by ID' });
+    }
 });
 
 //Express route to insert review
